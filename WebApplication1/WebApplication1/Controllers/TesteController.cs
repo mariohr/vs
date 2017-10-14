5d7156ad58ab1b6255a67a77f8966a31221780d4
Mario Henrique <mariohr@yahoo.com>
2017-10-14 16:47:22 -0300

    leitura dolar

--- WebApplication1/WebApplication1/Controllers/TesteController.cs
+++ WebApplication1/WebApplication1/Controllers/TesteController.cs
@@ -1,9 +1,11 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace WebApplication1.Controllers
@@ -14,6 +16,20 @@ namespace WebApplication1.Controllers
         public DateTime ts { get; set; }
     }
 
+    public class Dolar
+    {
+        public string nome { get; set; }
+        public float valor { get; set; }
+        public int ultima_consulta { get; set; }
+        public string fonte { get; set; }
+        public DateTime ts { get; set; }
+    }
+
+    public class Cotacao
+    {
+        public bool status { get; set; }
+        public List<Dolar> valores { get; set; }
+    }
 
     public class TesteController : ApiController
     {
@@ -40,13 +56,52 @@ namespace WebApplication1.Controllers
         // GET: api/Teste/resposta
         [Route("api/Teste/resposta")]
         [HttpGet]
-        public string GetResposta()
-       
-            Resposta resp = new Resposta();
-            resp.msg = "teste";
-            resp.ts = DateTime.Now;
+        public string GetResposta() {
+
+            Resposta resp = new Resposta
+            {
+                msg = "teste",
+                ts = DateTime.Now
+            };
+
             string s = JsonConvert.SerializeObject(resp);
             return s;
+        }
+
+        //
+        // GET: api/Teste/resposta
+        [Route("api/Teste/dolar")]
+        [HttpGet]
+        public async Task<Dolar> GetDolar()
+        {
+            string path = "http://api.promasters.net.br/cotacao/v1/valores";
+            HttpClient client = new HttpClient();
+            Dolar doleta = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                string ret = await response.Content.ReadAsStringAsync();
+                JObject googleSearch = JObject.Parse(ret);
+                doleta = googleSearch["valores"]["USD"].ToObject<Dolar>();
+
+                System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+                doleta.ts = dtDateTime.AddSeconds(doleta.ultima_consulta).ToLocalTime();
+            }
+/*
+            //var t1 = service.getdata1Async();
+            //var t2 = service.getdata2Async();
+            //var t3 = service.getdata3Async();
+            //await Task.WhenAll(t1, t2, t3);
+
+            var data = new returnObject
+            {
+                d1 = await t1,
+                d2 = await t2,
+                d3 = await t3
+            };
+
+            return Ok(data); */
+            return doleta;
         }
 
         // POST: api/Teste
